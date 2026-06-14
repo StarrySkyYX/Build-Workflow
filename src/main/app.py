9af7c6cc62ee7229bc916e3c09094c3bc5264e28
StarrySkyYX <s1447891024@gmail.com>
2026-06-15 03:44:41 +0800

    Add middleware setting

--- src/main/app.py
+++ src/main/app.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
 from datetime import datetime, timezone
-
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from src.main.model import GreetingResponse, HelloRequest
 
 app = FastAPI()
 
+# ======================
+# Security Middleware
+# ======================
 
+# CORS 設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # production 可改成指定 domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Trusted Host（防 host header attack）
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["*"]  # production 改成 your domain
+)
 
 @app.post("/hello", response_model=GreetingResponse)
 def hello(req: HelloRequest):
