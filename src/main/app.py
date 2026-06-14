1a0d007dcaeacd784ec51c4595216791de7485cf
StarrySkyYX <s1447891024@gmail.com>
2026-06-14 12:39:13 +0800

    Delete the hard-coding of token

--- src/main/app.py
+++ src/main/app.py
@@ -5,7 +5,6 @@ from src.main.model import GreetingResponse, HelloRequest
 
 app = FastAPI()
 
-# GITHUB_TOKEN = "ghp_R7kL9mX2pQ8nW4vY6sT1uZ3bC5dF0hJ"
 
 
 @app.post("/hello", response_model=GreetingResponse)
